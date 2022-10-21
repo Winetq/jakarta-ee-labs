0d994d9d3e5dcd94ad476d442dae834f93de0900
Winetq <winetq9679@gmail.com>
2022-10-21 21:28:50 +0200

    Initial config for jsf

---
+++ src/main/java/jakarta/ee/view/Config.java
@@ -0,0 +1,11 @@
+package jakarta.ee.view;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ * Config for JSF 2.3.
+ */
+@FacesConfig
+@ApplicationScoped
+public class Config {}
